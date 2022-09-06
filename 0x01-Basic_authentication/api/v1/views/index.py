d8b3ad9981fb656f5a4eee8ff95aa4aab6f1794b
Luis Mejia <luismejia69@gmail.com>
2022-09-06 11:43:46 -0500

    task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> None:
       - the unauthorized  status of the API
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> None:
+    """ GET /api/v1/forbidden
+    Return:
+      - the forbidden zone of the API
+    """
+    abort(403)
