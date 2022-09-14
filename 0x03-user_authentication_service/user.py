9e83e2f97f23417b52b4a3c7e9f419ee1e14b8c6
LEMM <luismejia69@gmail.com>
2022-09-14 12:16:36 -0500

    simple users table

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# engine = sqlalchemy.create_engine()
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(50), nullable=False, unique=True)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
