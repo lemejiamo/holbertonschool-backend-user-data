c8818bc1e863dde5ae4a2f046e97bff67a8b86e8
LEMM <luismejia69@gmail.com>
2022-09-14 18:18:10 -0500

    probando s uerte con el checker

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,8 +30,9 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Create a User and save the user to the database"""
-        newUser = User(email=email, hashed_password=hashed_password)
-        self._session.add(newUser)
+        """Create a User and save the user to the database
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return newUser
+        return new_user
